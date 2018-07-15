999d5b2948b6ad229a2028de220364a33102343e
chenjinkai <chenjkai@163.com>
2018-07-15 17:03:55 +0800

    hot moduloe replacement

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
+	mode: 'development',
 	entry: { 
 		app: './src/index.js'
 	},
@@ -19,10 +20,17 @@ module.exports = {
 		contentBase: './dist',
 		hot: true
 	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 		publicPath: '/'
-
 	}
 };
