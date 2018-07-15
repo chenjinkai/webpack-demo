6e072abb31af93606257b76e2a9506c5b1f53e3b
chenjinkai <chenjkai@163.com>
2018-07-15 13:16:42 +0800

    next guides warm up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  //add the nimage to our existing div
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
+
   return element;
 }
 
