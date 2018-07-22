7c9644e2ec3dd290094d35d1c3bece4744b3ea41
chenjinkai <chenjkai@163.com>
2018-07-22 20:26:27 +0800

    code splitting

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import _ from 'lodash';
-import printMe from './print.js';
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+async function component() {
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  var btn = document.createElement('button'); 
-  btn.onclick = printMe;
-  
-  element.appendChild(btn);
-
-  return element;
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+    var element = document.createElement('div');
+    var _ = _.default;
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+  }).catch(error => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
-
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: { 
-		app: './src/index.js',
-		print: './src/print.js',
-		another: './src/another-module.js'
+		app: './src/index.js'
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
@@ -16,11 +14,7 @@ module.exports = {
 	],
 	output: {
 		filename: '[name].bundle.js',
+		chunkFilename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
-	},
-	optimization: {
-		splitChunks: {
-			chunks: 'all'
-		}
 	}
 };
